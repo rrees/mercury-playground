c906ce4751503b9da182e59716cc1a08b8f2d1fd
Robert Rees <robert.rees@guardian.co.uk>
2014-12-30 17:29:19 +0000

    Cannabalise the example demo to create a greeter

--- scripts/greeter.js
+++ scripts/greeter.js
@@ -2,14 +2,51 @@
 var hg = require('mercury/index.js');
 var h = require('mercury/index.js').h;
 
-function App() {
+function HelloApp() {
 	return hg.struct({});
 }
 
-App.render = function render() {
+HelloApp.render = function render() {
 	return h('h1', 'Hello world')
 }
 
-console.log(hg);
+if(document.getElementById('hello')) {
+	hg.app(document.body, HelloApp(), HelloApp.render);
+}
 
-hg.app(document.body, App(), App.render);
+function GreetingsApp() {
+    var state = hg.struct({
+        text: hg.value(''),
+        handles: hg.value(null)
+    });
+
+	state.handles.set(
+		hg.handles({
+			change: function (state, data) {
+				state.text.set(data.text);
+			}
+		}, state));
+
+    return state;
+}
+
+GreetingsApp.render = function render(state) {
+    return h('div', [
+        h('p', [
+            'Greetings, ',
+			h('input', {
+				value: state.text,
+				name: 'text',
+				type: 'text',
+				placeholder: 'Enter your name',
+				'ev-event': hg.changeEvent(state.handles.change)
+				})
+        ]),
+        h('p', 'Hello, ' + state.text)
+
+    ]);
+};
+
+if(document.getElementById('greetings')) {
+	hg.app(document.body, GreetingsApp(), GreetingsApp.render);
+}
